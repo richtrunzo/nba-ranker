0a50ff0724926e4849ee4560a8b7c766e0da882a
richtrunzo <richtrunzo@gmail.com>
2020-12-16 18:08:34 +0000

    Quereied the dom for the login and sign up buttons and added event listeners to both. #2.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var $loginButton = document.querySelector('.login-btn');
+var $signupButton = document.querySelector('.signup-btn');
+
+$loginButton.addEventListener('click', function (event) {
+
+});
+
+$signupButton.addEventListener('click', function (event) {
+
+});
