a8e49de91db9d79ee1a8e42001c4e83ee175c492
richtrunzo <richtrunzo@gmail.com>
2020-12-16 18:13:45 +0000

    Added a form to the login page html and add a submit event listener to the form. #2.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 var $loginButton = document.querySelector('.login-btn');
 var $signupButton = document.querySelector('.signup-btn');
+var $loginForm = document.querySelector('.loginForm');
 
 $loginButton.addEventListener('click', function (event) {
 
 });
 
 $signupButton.addEventListener('click', function (event) {
+
+});
+
+$loginForm.addEventListener('submit', function (event) {
 
 });
