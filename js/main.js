3e081c58a5b95ef0d5a5862da426478151f780d7
richtrunzo <richtrunzo@gmail.com>
2020-12-16 18:16:39 +0000

    Added a form to the sign up page html and listened for submit events on the form in main.js. #2.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var $loginButton = document.querySelector('.login-btn');
 var $signupButton = document.querySelector('.signup-btn');
-var $loginForm = document.querySelector('.loginForm');
+var $loginForm = document.querySelector('.login-form');
+var $signupForm = document.querySelector('.signup-form');
 
 $loginButton.addEventListener('click', function (event) {
 
@@ -11,5 +12,9 @@ $signupButton.addEventListener('click', function (event) {
 });
 
 $loginForm.addEventListener('submit', function (event) {
+
+});
+
+$signupForm.addEventListener('submit', function (event) {
 
 });
