8399a8a9527c9a475545e97205928bc595fc19ad
richtrunzo <richtrunzo@gmail.com>
2020-12-16 20:49:22 +0000

    Appended profile data to the my profile page after sign up. #2.

--- js/main.js
+++ js/main.js
@@ -10,7 +10,16 @@ var $signupUsername = document.querySelector('.signup-username');
 var $signupPassword = document.querySelector('.signup-password');
 var $signupTeam = document.querySelector('.signup-team');
 
+var $myProfile = document.getElementById('my-profile');
+
 var $view = document.querySelectorAll('.view');
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  if (data.profile.username !== null) {
+    viewSwap(3);
+    $myProfile.appendChild(profileRender(data));
+  }
+});
 
 $loginButton.addEventListener('click', function (event) {
   viewSwap(1);
@@ -32,6 +41,7 @@ $signupForm.addEventListener('submit', function (event) {
   data.profile.team = $signupTeam.value;
   viewSwap(3);
   event.preventDefault();
+  $myProfile.appendChild(profileRender(data));
 });
 
 function viewSwap(view) {
@@ -39,4 +49,39 @@ function viewSwap(view) {
     $view[i].className = 'view hidden';
   }
   $view[view].className = 'view active';
+}
+
+function profileRender() {
+  var $container = document.createElement('div');
+
+  var $divOne = document.createElement('div');
+  $divOne.setAttribute('class', 'row-profile');
+  var $name = document.createElement('h3');
+  $name.appendChild(document.createTextNode('NAME:' + '   ' + data.profile.name));
+  $divOne.appendChild($name);
+
+  var $divTwo = document.createElement('div');
+  $divTwo.setAttribute('class', 'row-profile');
+  var $userName = document.createElement('h3');
+  $userName.appendChild(document.createTextNode('USERNAME:' + '   ' + data.profile.username));
+  $divTwo.appendChild($userName);
+
+  var $divThree = document.createElement('div');
+  $divThree.setAttribute('class', 'row-profile');
+  var $email = document.createElement('h3');
+  $email.appendChild(document.createTextNode('EMAIL:' + '   ' + data.profile.email));
+  $divThree.appendChild($email);
+
+  var $divFour = document.createElement('div');
+  $divFour.setAttribute('class', 'row-profile');
+  var $team = document.createElement('h3');
+  $team.appendChild(document.createTextNode('TEAM:' + '   ' + data.profile.team));
+  $divFour.appendChild($team);
+
+  $container.appendChild($divOne);
+  $container.appendChild($divTwo);
+  $container.appendChild($divThree);
+  $container.appendChild($divFour);
+
+  return $container;
 }
