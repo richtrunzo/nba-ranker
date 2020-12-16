7e6d6e55138cea050bfaa1ca3f943ab4d99dd4b3
richtrunzo <richtrunzo@gmail.com>
2020-12-16 19:22:15 +0000

    Added sign up profile data to local storage upon submission of the form. #2.

--- js/data.js
+++ js/data.js
@@ -6,5 +6,16 @@ var data = {
     username: null,
     password: null,
     team: null
-  }
+  },
+  rankings: []
 };
+
+var previousData = localStorage.getItem('profile-information');
+if (previousData !== null) {
+  data = JSON.parse(previousData);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('profile-information', dataJSON);
+});
--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 var $loginButton = document.querySelector('.login-btn');
 var $signupButton = document.querySelector('.signup-btn');
+
 var $loginForm = document.querySelector('.login-form');
+
 var $signupForm = document.querySelector('.signup-form');
+var $signupName = document.querySelector('.signup-name');
+var $signupEmail = document.querySelector('.signup-email');
+var $signupUsername = document.querySelector('.signup-username');
+var $signupPassword = document.querySelector('.signup-password');
+var $signupTeam = document.querySelector('.signup-team');
+
 var $view = document.querySelectorAll('.view');
 
 $loginButton.addEventListener('click', function (event) {
@@ -17,7 +25,13 @@ $loginForm.addEventListener('submit', function (event) {
 });
 
 $signupForm.addEventListener('submit', function (event) {
+  data.profile.name = $signupName.value;
+  data.profile.email = $signupEmail.value;
+  data.profile.username = $signupUsername.value;
+  data.profile.password = $signupPassword.value;
+  data.profile.team = $signupTeam.value;
   viewSwap(3);
+  event.preventDefault();
 });
 
 function viewSwap(view) {
